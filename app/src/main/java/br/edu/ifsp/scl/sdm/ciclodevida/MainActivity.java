9cf0f02577ef9e211ed5c468f12c9c937f38b99f
Vinicius <vinicius.zhu@aluno.ifsp.edu.br>
2018-08-24 21:50:48 -0300

    Adicionando métodos de fim de ciclo de vida

--- app/src/main/java/br/edu/ifsp/scl/sdm/ciclodevida/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/ciclodevida/MainActivity.java
@@ -12,7 +12,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // iniciando o ciclo de vida completo
+        // iniciando o ciclo de vida COMPLETO
         Log.v(CDV_LOGCAT_TAG, "onCreate: Iniciando ciclo COMPLETO");
     }
 
@@ -31,4 +31,37 @@ public class MainActivity extends AppCompatActivity {
         // Iniciando o ciclo de vida em PRIMEIRO PLANO
         Log.v(CDV_LOGCAT_TAG, "onResume: Iniciando ciclo em PRIMEIRO PLANO");
     }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        // Finalizando o ciclo de vida em PRIMEIRO PLANO
+        Log.v(CDV_LOGCAT_TAG, "onPause: Finalizando ciclo em PRIMEIRO PLANO");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // Finalizando o ciclo de vida VISÍVEL
+        Log.v(CDV_LOGCAT_TAG, "onStop: Finalizando ciclo VISÍVEL");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+
+        // Finalizando o ciclo de vida COMPLETO
+        Log.v(CDV_LOGCAT_TAG, "onDestroy: Finalizando ciclo COMPLETO");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        // Preparando execução do onStart
+        Log.v(CDV_LOGCAT_TAG, "onRestart: Preparando execução do onStart");
+    }
+
 }
