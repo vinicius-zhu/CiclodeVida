9f5cfcea5a0bbe03b217e662a6c6361671160255
Vinicius <vinicius.zhu@aluno.ifsp.edu.br>
2018-08-24 21:39:20 -0300

    Criação de métodos onCreate, onStart, onResume e adição de tags do LogCat

--- app/src/main/java/br/edu/ifsp/scl/sdm/ciclodevida/MainActivity.java
+++ app/src/main/java/br/edu/ifsp/scl/sdm/ciclodevida/MainActivity.java
@@ -2,12 +2,33 @@ package br.edu.ifsp.scl.sdm.ciclodevida;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
+    private final String CDV_LOGCAT_TAG = "CDV_LOGCAT_TAG";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // iniciando o ciclo de vida completo
+        Log.v(CDV_LOGCAT_TAG, "onCreate: Iniciando ciclo COMPLETO");
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+
+        // Iniciando o ciclo de vida VISÍVEL
+        Log.v(CDV_LOGCAT_TAG, "onStart: Iniciando ciclo VISÍVEL");
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+
+        // Iniciando o ciclo de vida em PRIMEIRO PLANO
+        Log.v(CDV_LOGCAT_TAG, "onResume: Iniciando ciclo em PRIMEIRO PLANO");
     }
 }
